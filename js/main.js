3340f0b8c62de97ac80755b57d368502c60dc19c
EC2 Default User <ec2-user@ip-172-31-13-114.ap-northeast-1.compute.internal>
2023-07-03 12:42:22 +0000

    アコディオンUI完成

--- js/main.js
+++ js/main.js
@@ -2,10 +2,16 @@
 
 {
   const dts = document.querySelectorAll('dt');
-  
+
   dts.forEach(dt => {
     dt.addEventListener('click', () => {
       dt.parentNode.classList.toggle('appear');
+
+      dts.forEach(el => {
+        if (dt !== el) {
+          el.parentNode.classList.remove('appear');
+        }
+      });
     });
   });
 }
